e4267cfe2343a94cf2d4b4574d54143946ae7587
Louise Reid <l_reid@hotmail.co.uk>
2018-06-21 14:37:57 +0100

    server set up

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const parser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+const publicPath = path.join(__dirname, '../client/public');
+app.use(express.static(publicPath));
+
+app.use(parser.json())
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  if(err){
+    console.log(err);
+  }
+
+  const db = client.db('oldedin');
+
+  const locations = db.collection('locations');
+  const locationsRouter = createRouter(locations);
+  app.use('/api/locations', locationsRouter);
+
+  const visits = db.collection('visits');
+  const visitsRouter = createRouter(visits);
+  app.use('/api/visits', visitsRouter);
+});
+
+
+
+app.listen(3000, function(){
+  console.log('listening on port 3000');
+})
