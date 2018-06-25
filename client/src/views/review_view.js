69a0c163f07deb30b4aac160d96d37dfeecc4ca2
Louise Reid <l_reid@hotmail.co.uk>
2018-06-25 14:39:28 +0100

    review submit passing through id

--- client/src/views/review_view.js
+++ client/src/views/review_view.js
@@ -3,28 +3,38 @@ const PubSub = require('../helpers/pub_sub.js');
 
 const ReviewView = function(form){
   this.form = form;
+  this.location = ''
 }
 
 ReviewView.prototype.bindEvents = function () {
+  this.storeLocation();
   this.form.addEventListener('submit', (evt) => {
     this.handleSubmit(evt);
   })
 };
 
+ReviewView.prototype.storeLocation = function () {
+  PubSub.subscribe('Itinerary:review-btn-clicked', (evt) => {
+    this.location = evt.detail
+  })
+};
+
 ReviewView.prototype.handleSubmit = function (evt) {
   evt.preventDefault();
-  const review = this.createReview(evt.target)
+  const review = this.createReview(evt.target, this.location)
   console.log(review);
   PubSub.publish('Review:review-submitted', review)
   evt.target.reset();
-
   const modal = document.getElementById('modal');
   modal.style.display = "none";
 };
 
-ReviewView.prototype.createReview = function (form) {
+ReviewView.prototype.createReview = function (form, location) {
   const review = {
-    review: form.review.value
+    id: location,
+    payload: {
+      review: form.review.value
+    }
   }
   return review
 };
